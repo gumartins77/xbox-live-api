29235c49ab2af730138c37d04a18cc1c69a7f5cd
Gustavo Martins <gugumartin77@gmail.com>
2022-05-26 18:10:35 -0300

    feat(user): implement bcrypt in password and cpf

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,17 +8,32 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+  private userSelect = {
+    id: true,
+    Name: true,
+    Email: true,
+    Password: false,
+    CPF: false,
+    isAdmin: true,
+    createdAt: true,
+    updatedAt: true,
+  };
+
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({ select: this.userSelect });
   }
 
   async findById(id: string): Promise<User> {
-    const record = await this.prisma.user.findUnique({ where: { id } });
+    const record = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
 
     if (!record) {
       throw new NotFoundException(`Record with Id '${id}' not found!`);
@@ -31,10 +46,16 @@ export class UserService {
     return this.findById(id);
   }
 
-  create(dto: CreateUserDto): Promise<User> {
-    const data: User = { ...dto };
+  async create(dto: CreateUserDto): Promise<User> {
+    const data: User = {
+      ...dto,
+      Password: await bcrypt.hash(dto.Password, 10),
+      CPF: await bcrypt.hash(dto.CPF, 10),
+    };
 
-    return this.prisma.user.create({ data }).catch(this.handleError);
+    return this.prisma.user
+      .create({ data, select: this.userSelect })
+      .catch(this.handleError);
   }
 
   async update(id: string, dto: UpdateUserDto) {
@@ -42,10 +63,19 @@ export class UserService {
 
     const data: Partial<User> = { ...dto };
 
+    if (data.Password) {
+      data.Password = await bcrypt.hash(data.Password, 10);
+    }
+
+    if (data.CPF) {
+      data.CPF = await bcrypt.hash(data.CPF, 10);
+    }
+
     return this.prisma.user
       .update({
         where: { id },
         data,
+        select: this.userSelect,
       })
       .catch(this.handleError);
   }
