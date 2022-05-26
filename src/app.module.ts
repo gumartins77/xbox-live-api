524d9a1b3491911519cb8f71dbd14d83748eba5e
Gustavo Martins <gugumartin77@gmail.com>
2022-05-26 16:24:28 -0300

    feat(user): generate resource, implement controller, dto, service & validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { GameModule } from './game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { GenderModule } from './gender/gender.module';
 import { ProfileModule } from './profile/profile.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [GameModule, PrismaModule, GenderModule, ProfileModule],
+  imports: [GameModule, PrismaModule, GenderModule, ProfileModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,57 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The name of the user.',
+    example: 'Phil Spencer',
+  })
+  Name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The email of the user.',
+    example: 'pspencer@gmail.com',
+  })
+  Email: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
+  @ApiProperty({
+    description: 'The password of the user.',
+    example: 'Xbox@1234',
+  })
+  Password: string;
+
+  @IsString()
+  @MinLength(11)
+  @MaxLength(14)
+  @ApiProperty({
+    description: 'The cpf of the user.',
+    example: '123.456.789-10',
+  })
+  CPF: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Confirmation whether you are an admin or not.',
+    example: true,
+  })
+  isAdmin: boolean;
+}
